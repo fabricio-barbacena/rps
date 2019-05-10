4fdd9b32d652b08fcb605dfb92849d7032e0f561
Fabrício Santos Barbacena <fabricio.barbacena@capes.gov.br>
2019-05-10 14:17:33 -0300

    Initial commit

---
+++ rps_novo.py
@@ -0,0 +1,448 @@
+import random
+import time
+import string
+import math
+import itertools
+import pprint
+
+
+"""This program plays a game of Rock, Paper, Scissors between two Players,
+and reports both Player's scores each round."""
+
+moves = ['rock', 'paper', 'scissors']
+
+"""The Player class is the parent class for all of the Players
+in this game"""
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.my_move_recorder = None
+        self.enemy_move_recorder = None
+
+    def move(self):
+        move = moves[(random.randint(0,2))]
+        self.my_move_recorder = move
+        return self.my_move_recorder
+
+    def learn(self, my_move, enemy_move):
+        self.my_move_recorder = my_move
+        self.enemy_move_recorder = enemy_move
+        return self.enemy_move_recorder
+
+
+class Same_move_player(Player):
+    def __init__(self, name):
+        Player.__init__(self, name)
+        self.my_move_recorder = random.choice(moves)
+
+    def move(self):
+        return self.my_move_recorder
+
+
+class Rock_player(Same_move_player):
+    def __init__(self, name):
+        Player.__init__(self, name)
+        self.my_move_recorder = 'rock'
+
+    def move(self):
+        return self.my_move_recorder
+
+
+class Cyclic_player(Player):
+    def __init__(self, name):
+        Player.__init__(self, name)
+        self.setup_executed = False
+        self.my_move_recorder = random.choice(moves)
+
+    def setup_choice(self):
+        self.setup_executed = True
+        return self.my_move_recorder
+
+    def move(self):
+        if self.my_move_recorder == 'rock':
+            self.my_move_recorder = 'paper'
+            return 'paper'
+        elif self.my_move_recorder == 'paper':
+            self.my_move_recorder = 'scissors'
+            return 'scissors'
+        else:
+            self.my_move_recorder = 'rock'
+            return 'rock'
+
+
+class Human_player(Player):
+    def __init__(self, name=None):
+        Player.__init__(self, name)
+        self.name = input("\nWhat player's name do you choose for yourself? ")
+
+    def move(self):
+        move = (input(f'{self.name.upper()}, time to play!\n\n'
+                      '\tRock, paper, scissors? > ').
+                strip(string.punctuation + ' ').lower())
+        while move not in moves:
+            """if move in ['quit', 'exit', 'leave']:
+                print('\n leaving the game...\n...\n')
+                break"""
+            move = (input("\tRock, paper, scissors? > ").
+                    strip(string.punctuation + ' ').lower())
+        self.my_move_recorder = move
+        return self.my_move_recorder
+
+
+class Copycat_player(Player):
+    def __init__(self, name):
+        Player.__init__(self, name)
+        self.first_movement = False
+        self.my_move_recorder = None
+        self.enemy_move_recorder = None
+
+    def first_move(self):
+        self.first_movement = True
+        self.my_move_recorder = random.choice(moves)
+        return self.my_move_recorder
+
+    def move(self):
+        if self.first_movement == False:
+            return self.first_move()
+        else:
+            move = self.enemy_move_recorder
+            return move
+
+
+class Game:
+    def __init__(self, p, name, print_slow=True):
+        self.p = p
+        self.name = name
+        self.print_slow = print_slow 
+
+    def print_speed(self, str):
+        if self.print_slow == True:
+            print(str)
+            time.sleep(2)
+        else:
+            print(str)
+
+    def check_players(self):
+        if len(self.p) != 2:
+            print('We need 2 players in the Game, no more, no less.\n'
+                  'Please reset the game.\n')
+            return 'not OK'
+
+
+    def play_round(self):
+        move1 = self.p[0].move()
+        move2 = self.p[1].move()
+        self.print_speed(f'\n{self.p[0].name}: {move1}  '
+                         f'{self.p[1].name}: {move2}')
+        self.p[0].move_recorder = move1  # ver o que  essas duas linhas
+        self.p[1].move_recorder = move2  # estão fazendo
+        winner = self.round_winner(move1, move2)
+        self.p[0].learn(move1, move2)
+        self.p[1].learn(move2, move1)
+        if winner == 'tie':
+            self.print_speed(f'\n** IT IS A TIE! **\n')
+        else:
+            self.print_speed(f'\n** {winner.upper()} WINS! **\n')
+            return winner 
+
+
+    def round_winner(self, move1, move2):
+        if move1 == move2:
+            return 'tie'
+        elif (move1 == 'rock' and move2 == 'scissors' or
+              move1 == 'scissors'and move2 == 'paper' or
+              move1 == 'paper'and move2 == 'rock'):
+            return self.p[0].name
+        else:
+            return self.p[1].name
+
+    def play_game(self):
+        self.print_speed(f'\n{self.name.upper()}\n')
+        self.print_speed('\nGame start!\n')
+        number_players = self.check_players()
+        if number_players == 'not OK':
+            return
+        else:
+            self.play_round()
+            print()
+            self.print_speed('Game over!\n')
+
+
+class Game_rounds(Game):
+    def __init__(self, p, name, rounds, print_slow=True):
+        Game.__init__(self, p, name)
+        self.rounds = rounds
+        self.print_slow = print_slow
+        self.p[0].wins = 0
+        self.p[1].wins = 0
+
+    def introduction(self):
+        self.print_speed(f'This game has {self.rounds} rounds.\n')
+        self.print_speed('The player who has more victories after '
+                         'all the rounds is the final winner!\n')
+        self.print_speed('If the players have the same number of victories '
+                         'by the end of the game,')
+        self.print_speed('it will be declared a draw.\n\n')
+
+    def play_game(self):
+        self.print_speed(f'\n{self.name.upper()}\n')
+        self.print_speed('\nGame start!\n')
+        number_players = self.check_players()
+        if number_players == 'not OK':
+            return
+        else:
+            self.introduction()
+            for round in range(self.rounds):
+                self.print_speed(f'Round {str(round+1)}:\n')
+                winner = self.play_round()
+                print()
+                if winner == self.p[0].name:
+                    self.p[0].wins += 1
+                elif winner == self.p[1].name:
+                    self.p[1].wins += 1            
+                else:
+                    pass
+            self.print_speed('Game over!\n')
+            self.final_winner()
+
+    def final_winner(self):
+        if self.p[0].wins > self.p[1].wins:
+            self.print_speed(f'FINAL SCORE\nAFTER {self.rounds} ROUNDS:\n')
+            self.print_speed(f'{self.p[0].name}: {self.p[0].wins}, '
+                             f'{self.p[1].name}: {self.p[1].wins}\n\n')
+            self.print_speed(f'** !!! {(self.p[0].name).upper()} '
+                             f'WINS THE GAME !!! **\n')
+        elif self.p[0].wins < self.p[1].wins:
+            self.print_speed(f'FINAL SCORE\nAFTER {self.rounds} ROUNDS:\n')
+            self.print_speed(f'{self.p[0].name}: {self.p[0].wins}, '
+                             f'{self.p[1].name}: {self.p[1].wins}\n\n')
+            self.print_speed(f'** !!! {(self.p[1].name).upper()} '
+                             f'WINS THE GAME !!! **\n')
+        else:
+            self.print_speed(f'FINAL SCORE\nAFTER {self.rounds} ROUNDS:\n')
+            self.print_speed(f'{self.p[0].name}: {self.p[0].wins}, '
+                             f'{self.p[1].name}: {self.p[1].wins}\n\n')
+            self.print_speed(f'** !!! THE GAME ENDED IN A DRAW !!! **\n')
+
+
+class Game_wins(Game):
+    def __init__(self, p, name, wins, print_slow=True ):
+        Game.__init__(self, p, name)
+        self.wins = wins
+        self.print_slow = print_slow
+        self.round = 1
+        self.p[0].wins = 0
+        self.p[1].wins = 0
+
+    def introduction(self):
+        self.print_speed(f'The player who gets {self.wins} victories first '
+                         'wins the Game!\n')
+        self.print_speed('There is no limit of rounds here.\n')
+        self.print_speed('However, if the players keep playing the same moves '
+                         'over and over again, the game will come to an end '
+                         'at the 50th round.\n')
+
+    def play_game(self):
+        self.print_speed(f'\n{self.name.upper()}\n')
+        self.print_speed('\nGame start!\n')
+        number_players = self.check_players()
+        if number_players == 'not OK':
+            return
+        else:
+            self.introduction()
+            while self.p[0].wins < self.wins and self.p[1].wins < self.wins:
+                if self.round > 50:
+                    break
+                self.print_speed(f'Round {str(self.round)}:\n')
+                winner = self.play_round()
+                print()
+                if winner == self.p[0].name:
+                    self.p[0].wins += 1
+                    winner_end = self.p[0]
+                elif winner == self.p[1].name:
+                    self.p[1].wins += 1
+                    winner_end = self.p[1]
+                else:
+                    pass
+                self.print_speed(f'{self.p[0].name}: {self.p[0].wins}, '
+                                 f'{self.p[1].name}: {self.p[1].wins}\n\n')
+                self.round += 1
+            self.print_speed('Game over!\n')
+            self.final_winner()
+            self.p[0].wins = 0
+            self.p[1].wins = 0
+            return winner_end
+
+
+    def final_winner(self):
+        if self.p[0].wins == self.wins:
+            self.print_speed(f'FINAL SCORE \nAFTER {self.round-1} ROUNDS:\n')
+            self.print_speed(f'{self.p[0].name}: {self.p[0].wins}, '
+                             f'{self.p[1].name}: {self.p[1].wins}\n\n')
+            self.print_speed(f'** !!! {(self.p[0].name).upper()} '
+                             f'WINS THE GAME !!! **\n')
+        elif self.p[1].wins == self.wins:
+            self.print_speed(f'FINAL SCORE \nAFTER {self.round-1} ROUNDS:\n')
+            self.print_speed(f'{self.p[0].name}: {self.p[0].wins}, '
+                             f'{self.p[1].name}: {self.p[1].wins}\n\n')
+            self.print_speed(f'** !!! {(self.p[1].name).upper()} '
+                             'WINS THE GAME !!! **\n')
+        else:
+            self.print_speed(f'FINAL SCORE \nAFTER {self.round-1} ROUNDS:\n')
+            self.print_speed(f'{self.p[0].name}: {self.p[0].wins}, '
+                             f'{self.p[1].name}: {self.p[1].wins}\n\n')
+            self.print_speed(f'** !!! THE GAME ENDED IN A DRAW !!! **\n')
+
+
+class Championship():
+    def __init__(self, p, name):
+        self.p = p
+        self.name = name
+        self.n_phases = int(math.log(len(p), 2))
+        self.print_slow = True
+        self.game_print_slow = True
+        self.players_phase = p
+        self.phase_names = []
+
+    def print_speed(self, str):
+        if self.print_slow == True:
+            print(str)
+            time.sleep(2)
+        else:
+            print(str)
+
+    def check_players(self):
+        if len(self.p) not in [2**x for x in range(1,12)]:
+            print('\nFor this kind of Championship, we need the number of '
+                  'players to be one of the following:\n\n'
+                  '2, 4, 8, 16, 32, 64, 128, 256, 512, 1024 or 2048.\n\n'
+                  'Please reset the game.\n')
+            return 'not OK'
+
+    def name_of_phases(self):
+        for index in range(self.n_phases):
+            self.phase_names.append('Phase ' + str(index+1))
+        if len(self.phase_names) == 1:
+            self.phase_names[-1] += ' (Final)'
+        elif len(self.phase_names) == 2:
+            self.phase_names[-1] += ' (Final)'
+            self.phase_names[-2] += ' (Semi-Finals)'
+        else:
+            self.phase_names[-1] += ' (Final)'
+            self.phase_names[-2] += ' (Semi-Finals)'
+            self.phase_names[-3] += ' (Quarter-Finals)'
+
+    def set_phase(self):
+        self.games = []
+        self.pair_players = [[self.players_phase[x],self.players_phase[x+1]]
+                             for x in range(0,int(len(self.players_phase)),2)]
+        for index in range(len(self.pair_players)):
+            name = 'Game ' + str(index+1)
+            self.games.append(Game_wins(self.pair_players[index], name,
+                                        3, self.game_print_slow))
+        return self.games
+
+    def play_phase(self):
+        self.games = self.set_phase()
+        self.winners = []
+        for game in self.games:
+            winner = game.play_game()
+            self.winners.append(winner)
+        self.players_phase = self.winners
+
+    def play_championship(self):
+        check_players = self.check_players()
+        if check_players == 'not OK':
+            return
+        self.name_of_phases()
+        for phase in range(self.n_phases):
+            self.print_speed(f'\n{self.phase_names[phase].upper()}')
+            self.play_phase()
+        self.declare_champion()
+
+    def declare_champion(self):
+        self.print_speed(f'End of the {self.name}!\n')
+        self.print_speed('The final champion is')
+        self.print_speed(f'\n** {self.players_phase[0].name}**\n')
+        self.print_speed('CONGRATULATIONS, '
+                         f'{self.players_phase[0].name.upper()}!\n') 
+
+
+class Championship_points(Championship):
+    def __init__(self, p, name):
+        Championship.__init__(self, p, name)
+        self.print_slow = True
+        self.game_print_slow = True
+        self.points = {}
+        self.players_pairs = []
+        self.games = []
+
+    def print_speed(self, str):
+        if self.print_slow == True:
+            print(str)
+            time.sleep(2)
+        else:
+            print(str)
+
+    def set_players_in_game(self):
+        self.players_pairs = list(itertools.combinations(self.p, 2))
+        pprint.pprint(self.players_pairs)
+        for player in self.p:
+            self.points.setdefault(player.name, 0)
+
+    def set_games(self):
+        for pair in self.players_pairs:
+            self.games.append(Game_wins(pair, 'Game ' +
+                                          str(self.players_pairs.index(pair)+1),
+                                          3, self.game_print_slow))
+
+    def play_championship(self):
+        points = self.points
+        self.set_players_in_game()
+        self.set_games()
+        for game in self.games:
+            winner = game.play_game()
+            points[winner.name] += 1
+            self.sorting_wins
+        self.points = points
+        self.sorting_wins()
+
+    def sorting_wins(self):
+        final = sorted(self.points.items(), reverse=True,
+                       key = lambda kv:(kv[1], kv[0]))
+        print()
+        print(final)
+
+
+
+
+if __name__ == '__main__':
+    p1 = Player('p1')
+    p2 = Player('p2')
+    p3 = Player('p3')
+    p4 = Human_player('p4')
+
+    players = [p1, p2, p3, p4]
+
+    champ = Championship_points(players, 'Champ')
+
+    champ.play_championship()
+
+
+
+"""
+if __name__ == '__main__':
+    number_of_players = 4
+    players = []
+    for n in range(number_of_players - 1):
+        players.append(Player('Player ' + str(n+1)))
+    players.append(Human_player())
+    champ = Championship(players, 'Championship')
+    if number_of_players > 32:
+        champ.print_slow = False
+        champ.game_print_slow = False
+    champ.play_championship()
+"""
+
---
+++ rps_test.py
@@ -0,0 +1,107 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.shape('turtle')
+
+t.goto(100,100)
+t.width(10)
+t.pencolor('red')
+t.fillcolor('black')
+t.fd(100)
+t.color('blue')
+
+"""
+
+
+import shutil
+import os
+
+
+columns = shutil.get_terminal_size().columns
+print("hello world".center(columns))
+
+for x in range(10):
+    print(str(x).center(columns))
+
+print(os.getcwd())
+
+
+
+import rps_novo as rps
+
+dictio = {'a': 2,
+          'b': 1,
+          'c': 3,
+          'd': 4,
+          'e': 5,
+          'f': 6         
+          }
+
+lista = []
+
+   
+    #lista.append(dictio.popitem(max(item.values())))
+
+def takeSecond(elem):
+    return elem[1]
+
+for item in dictio.items():
+    lista.append(item)
+
+sort = sorted(lista, reverse= True, key=takeSecond)
+print(sort)
+
+
+
+
+p1 = rps.Same_move_player('Non sense')
+p2 = rps.Copycat_player('Rocky')
+
+jogo = rps.Game_wins([p1,p2], "Final Game!", 5, False)
+
+jogo.play_game()
+
+
+p1 = rps.Player('p1')
+p2 = rps.Player('p2')
+p3 = rps.Player('p3')
+p4 = rps.Player('p4')
+p5 = rps.Player('p5')
+p6 = rps.Player('p6')
+p7 = rps.Player('p7')
+p8 = rps.Human_player('p8')
+
+
+players = [p1, p2, p3, p4, p5, p6, p7, p8]
+
+
+champ = rps.Championship(players, "Championship")
+
+
+champ.play_championship()
+
+
+
+
+
+
+p = [str(x) + 'p' for x in range(1,9)]
+games = {}
+gamelist = []
+pair_players = [[p[x],p[x+1]] for x in range(0,int(len(p)),2)]
+for index in range(len(pair_players)):
+    gamelist.append(rps.Game(pair_players[index]))
+print(gamelist)
+
+"""    
+        
+        
+
+
+
+
+
+
+
+
